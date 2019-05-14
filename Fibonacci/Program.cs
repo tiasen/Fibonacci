95616781e03f1ae693e8b70b34d519320e238b24
Sen Tian <sen.tian@thoughtworks.com>
2019-05-14 15:10:22 +0800

    Fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+        
+        public static int GetResult(int number)
+        {
+            if (number <= 0)
+            {
+                return 0;
+            }
+            else if (number == 1 || number == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return GetResult(number - 1) + GetResult(number - 2);
+            }
+        }
+
+        public static ulong[] GetValueCache(int length)
+        {
+
+            var list = new ulong[length + 1];
+            for (int i = 0; i <= length; i++)
+            {
+                if (i == 0)
+                {
+                    list[i] = 0;
+                }
+                else if (i == 1 || i == 2)
+                {
+                    list[i] = 1;
+                }
+                else
+                {
+                    list[i] = list[i - 1] + list[i - 2];
+                }
+            }
+
+            return list;
+        }
+
+
+        public static ulong GetResultNew(int number)
+        {
+            return number < 0 ? 0 : GetValueCache(number)[number];
+        }
+    }
+}
---
+++ FibonacciTest/Tests.cs
@@ -0,0 +1,77 @@
+﻿using Fibonacci;
+using Xunit;
+
+namespace FibonacciTest
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void OutputFirst()
+        {
+            var result = Program.GetResult(1);
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void OutputSecond()
+        {
+            var result = Program.GetResult(number: 2);
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void OutputThird()
+        {
+            var result = Program.GetResult(3);
+            Assert.Equal(2, result);
+        }
+
+
+        [Theory]
+        [InlineData(-1, 0)]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        [InlineData(2, 1)]
+        [InlineData(3, 2)]
+        [InlineData(5, 5)]
+        [InlineData(10, 55)]
+        public void OutputN(int value, int except)
+        {
+            var result = Program.GetResult( value);
+            Assert.Equal(except, result);
+        }
+
+        [Fact]
+        public void GetResultCache()
+        {
+            var valueCache = Program.GetValueCache(10);
+            Assert.Equal(11, valueCache.Length);
+        }
+
+        [Theory]
+        [InlineData(-1, 0)]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        [InlineData(2, 1)]
+        [InlineData(3, 2)]
+        [InlineData(5, 5)]
+        [InlineData(10, 55)]
+        [InlineData(50, 12586269025)]
+        [InlineData(92, 7540113804746346429)]
+        public void GetResultNew(int value, ulong except)
+        {
+            var resultNew = Program.GetResultNew(value);
+            Assert.Equal(except, resultNew);
+        }
+        
+        
+
+    }
+    
+}
